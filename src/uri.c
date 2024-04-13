0217e3e8f5c9b9856bc6f77ccbbe4864ee224a3a
Lukas Krickl <lukas@krickl.dev>
2024-04-13 15:29:35 +0200

    WIP: url unescape test

--- src/uri.c
+++ src/uri.c
@@ -1,1 +1,69 @@
 #include "uri.h"
+#include "ugem.h"
+#include <ctype.h>
+
+struct ugem_uri ugem_uri_parse(const char *uri_str, unsigned long n) {
+  struct ugem_uri uri;
+
+  return uri;
+}
+
+int ugem_uri_reserved(int c) {
+  switch (c) {
+    case ';':
+    case '/':
+    case '?':
+    case '@':
+    case '&':
+    case '=':
+      return 1;
+    default:
+      break;
+  }
+
+  return 0;
+}
+
+unsigned long ugem_uri_unescape(char *dst, const char *src, unsigned long n) {
+  const char uri_escape_char = '%';
+
+  unsigned long writeat = 0;
+  unsigned long readat = 0;
+
+  for (readat = 0; readat < n; readat++) {
+    char c = src[readat];
+    if (c == uri_escape_char) {
+      if (readat + 3 < n) {
+        goto fail;
+      }
+
+      char hexstr[3] = { src[readat+1], src[readat+2], '\0'};
+    
+      if (!isxdigit(hexstr[0]) || !isxdigit(hexstr[1])) {
+        goto fail;
+      }
+
+      c = (char)strtol(hexstr, NULL, 16);
+
+      readat += 2; // + 2 to consume the 2 hex chars 
+
+    } 
+    dst[writeat++] = c;
+    
+  }
+
+  dst[writeat] = '\0';
+
+  return 0;
+
+fail:
+
+  return readat;
+}
+
+const char *ugem_uri_escape(const char *src, unsigned long n) {
+  
+}
+
+void ugem_uri_free(struct ugem_uri *uri) {
+}
--- src/uri.h
+++ src/uri.h
@@ -8,7 +8,7 @@ struct ugem_query {
 
 struct ugem_uri {
   const char *scheme;
-  const char *domain;
+  const char *authority;
 
   int port;
 
@@ -16,6 +16,19 @@ struct ugem_uri {
 
   struct ugem_query *query;
   unsigned long query_len;
+
+  const char *fragment;
 };
+
+struct ugem_uri ugem_uri_parse(const char *uri_str, unsigned long n);
+
+// unescapes a string if n size
+// On success 0 is returned, on failure 
+// the last read index of src is returned.
+// src and dst must be of size n+1.
+// dst is automatically terminated with \0
+unsigned long ugem_uri_unescape(char *dst, const char *src, unsigned long n);
+
+void ugem_uri_free(struct ugem_uri *uri);
 
 #endif 
